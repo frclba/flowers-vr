57c9095e98a83ed2e07becfb00129f4df21ac67d
Fernando Aguilar <fernando@aguilar.net.br>
2018-09-06 17:07:33 -0300

    Add boxes && animations

--- A-Frame-Demo/sketch.js
+++ A-Frame-Demo/sketch.js
@@ -1,30 +1,84 @@
 let sphere;
-let plane;
-let degrees = 0;
+let scene;
+let box_arr = [];
+
 let theta = 0;
+let degrees = 0;
 
 const shiftDegrees = (value) => (value + 1) % 360;
 
 function setup() {
-  sphere = select("#sphere");
-  // plane = select("#plane");
+  scene = select("#scene");
+  
+  spawn_boxes();
+
+  sphere = new Sphere(color(0, 0, 0), `-3 2.1 -3`)
 }
 
 
 function draw() {
+  const variation = sin(theta);
   degrees = shiftDegrees(degrees);
 
-  const color = `hsl(${degrees}, 100%, 50%)`;
-  
-  const variation = sin(theta);
-  const position = `-3 ${2.1 + variation} -3`;
-  const rotation = `-90 0 ${degrees}`;
+  sphere.move(variation);
 
-  sphere.attribute('color', color);
-  sphere.attribute('position', position);
-
-  // plane.attribute('color', color);
-  // plane.attribute('rotation', rotation);
+  box_arr.forEach(box => {
+    box.move(variation * 0.1);
+  });
 
   theta += 0.1;
+}
+
+function spawn_boxes() {
+  let box_position = 0;
+
+  for (let i = 0; i < 6; i++) {
+    let box_color = color(random(0, 255), random(0, 255), random(0, 255));
+
+    let newBox = new Box(box_color, box_position);
+    box_arr.push(newBox);
+    
+    box_position += 3;
+  }
+}
+
+class Box {
+  constructor(color, position) {
+    this.color = color;
+    this.position = position;
+
+    this.element = createElement('a-box');
+    this.element.attribute("position", `${this.position} 1.5  -3`);
+    this.element.attribute("color", this.color);
+
+    scene.child(this.element);
+  }
+
+  move(variation) {
+    this.position += variation;
+
+    this.element.attribute('position', `${this.position} 1.5  -3`);
+  }
+}
+
+class Sphere {
+  constructor(color, position) {
+    this.color = color || color(0, 0, 0);
+    this.position = position || `-3 ${2.1} -3`;
+
+    this.element = createElement('a-sphere');
+    this.element.attribute('color', color);
+    this.element.attribute('position', position);
+
+    scene.child(this.element);
+  }
+
+
+  move(variation) {
+    const color = `hsl(${degrees}, 100%, 50%)`;
+    const position = `-3 ${2.1 + variation} -3`;
+
+    this.element.attribute('position', position);
+    this.element.attribute('color', color)
+  }
 }
