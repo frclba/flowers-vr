29ef8855a0be9d414221a754a0d135c75402a9d2
Fernando Aguilar <fernando@aguilar.net.br>
2018-09-08 03:29:48 -0300

    Fixing project deployment errors

--- A-Frame-Demo/sketch.js
+++ A-Frame-Demo/sketch.js
@@ -5,13 +5,15 @@ let box_arr = [];
 let theta = 0;
 let degrees = 0;
 
+const $ = (query) => document.querySelector(query);
 const shiftDegrees = (value) => (value + 1) % 360;
 
+function preload(){
+   scene = select('a-scene');
+}
 
 function setup() {
-  scene = select("#scene");
-  spawn_boxes();
-
+  // spawn_boxes();
   sphere = new Sphere(color(0, 0, 0), `-3 2.1 -3`);
 }
 
@@ -23,7 +25,7 @@ function draw() {
   sphere.move(variation);
 
   box_arr.forEach(box => {
-    // box.move(variation * 0.1);
+    box.move(variation * 0.01);
   });
 
   theta += 0.1;
@@ -33,11 +35,55 @@ function spawn_boxes() {
   let box_position = 0;
 
   for (let i = 0; i < 33; i++) {
-    let box_color = color(random(0, 255), random(0, 255), random(0, 255));
+    // let box_color = color(random(0, 255), random(0, 255), random(0, 255));
 
-    let newBox = new Box(box_color, box_position);
+    let newBox = new Box("#FFF", box_position);
     box_arr.push(newBox);
     
     box_position += 3;
+  }
+}
+
+
+class Box {
+  constructor(color, position) {
+
+    this.color = color;
+    this.position = position;
+    this.sourceId = floor(random(1, 6));
+
+    this.element = createElement('a-box');
+    this.element.attribute("position", `3 1.5 -${this.position}`);
+    this.element.attribute("color", this.color);
+    this.element.attribute("src", `#tile${this.sourceId}`);
+
+    scene.child(this.element);
+  }
+
+  move(variation) {
+    this.position += variation;
+    this.element.attribute('position', `3 1.5 -${this.position}`);
+  }
+}
+
+class Sphere {
+  constructor(color, position) {
+    this.color = color || color(0, 0, 0);
+    this.position = position || `-3 ${2.1} -3`;
+
+    this.element = createElement('a-sphere');
+    this.element.attribute('color', color);
+    this.element.attribute('position', position);
+
+    scene.child(this.element);
+  }
+
+
+  move(variation) {
+    const color = `hsl(${degrees}, 100%, 50%)`;
+    const position = `-3 ${2.1 + variation} -3`;
+
+    this.element.attribute('position', position);
+    this.element.attribute('color', color)
   }
 }
